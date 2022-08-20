1e02176a8092fbc41a470948918101d7f9d35005
tomasfmarsico <tomasfmarsico@gmail.com>
2022-08-20 20:15:46 -0300

    add components footer navbar title

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/NavBar";
 
 const App = () => {
 	const persona = { nombre: "Tomas", altura: "184cm" };
 	return (
-		<div className="App">
+		<div className="App" style={{fontSize: "20px", borderBottom: "1px solid red"}}>
+			<Navbar />
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Hello World! {persona.nombre} mide {persona.altura}
 				</p>
-				{/* {ButtonComponent()} */}
 				<ButtonComponent />
 			</header>
 		</div>
@@ -18,7 +19,10 @@ const App = () => {
 };
 
 const ButtonComponent = () => {
-	return <button>Hola Mundo</button>;
+	const showAlert = (e) => {
+		alert("Esto es un alerta")
+	}
+	return <button onClick={showAlert}>Hola Mundo</button>;
 };
 
 export default App;
